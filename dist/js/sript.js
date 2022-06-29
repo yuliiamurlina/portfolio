e2b4599a45d2aedfc1d86f2db04352fd90eaeef4
yuliamurlina <y4liamur@gmail.com>
2022-06-29 19:23:28 +0300

    mailer

--- dist/js/sript.js
+++ dist/js/sript.js
@@ -13,4 +13,22 @@ const counter = document.querySelectorAll('.percent-item__number'),
     lines = document.querySelectorAll('.percent-item__red');
 counter.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
-} )
+});
+
+
+$('.contact__form').submit(function(e) {
+        e.preventDefault();
+        // if (!$(this).valid()) {
+        //     return;
+        // }
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            
+            $('form').trigger('reset');
+        });
+        return false;
+    });
---
+++ dist/mailer/smart.php
@@ -0,0 +1,44 @@
+<?php 
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'yulya.murlina@mail.ru';                 // Наш логин
+$mail->Password = '0ZdpYtA3E9XnGqDagJKb';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('yulya.murlina@mail.ru', 'Portfolio');   // От кого письмо 
+$mail->addAddress('higak24195@lankew.com');   // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	E-mail: ' . $email . ' <br>
+	Текст: ' . $text;
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
